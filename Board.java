28410a601a6bcd3b4f69cd844352199be1d0f685
RasmusTangaa <57133082+RasmusTangaa@users.noreply.github.com>
2019-12-12 20:31:01 +0100

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,21 @@
+public class Board{
+private Player player
+private Player autoPlayer
+public Board(Player playerVar1, Player playerVar2) {
+
+}
+public Card trumpCard(){
+	
+}
+public Player next() {
+	
+}
+
+public Player move(Card cardVar1, Card cardVar2) {
+	
+}
+public boolean gameOver() {
+	
+}
+
+}
