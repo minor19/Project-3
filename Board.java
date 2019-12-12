9be376ec9d693f0a38a639887645e016e5ad693d
RasmusTangaa <57133082+RasmusTangaa@users.noreply.github.com>
2019-12-12 22:22:55 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,10 +3,13 @@ private Player player;
 private Player autoPlayer;
 private Card trumpCard;
 private Player nextPlayer;
+private Player secondPlayer;
 
 public Board(Player playerVar1, Player playerVar2) {
 this.player = playerVar1;
 this.autoPlayer = playerVar2;
+this.nextPlayer = playerVar1;
+this.secondPlayer = playerVar2;
 }
 public Card trumpCard(){
 return this.trumpCard;	
