2e8f986fd28625366879455ee240cb943921b281
RasmusTangaa <57133082+RasmusTangaa@users.noreply.github.com>
2019-12-12 20:41:04 +0100

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,14 +1,18 @@
 public class Board{
-private Player player
-private Player autoPlayer
-public Board(Player playerVar1, Player playerVar2) {
+private Player player;
+private Player autoPlayer;
+private Card trumpCard;
+private Player nextPlayer;
 
+public Board(Player playerVar1, Player playerVar2) {
+this.player = playerVar1;
+this.autoPlayer = playerVar2;
 }
 public Card trumpCard(){
-	
+return this.trumpCard;	
 }
 public Player next() {
-	
+return this.nextPlayer;	
 }
 
 public Player move(Card cardVar1, Card cardVar2) {
